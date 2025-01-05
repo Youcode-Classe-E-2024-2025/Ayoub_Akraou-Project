8f3af174e3950233327c176b7fc9eaad49806688
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-01-05 19:58:08 +0100

    creer une component head pour etre réutilisable danst toutes les vues

---
+++ views/components/head.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" class="sm:text-[14px]">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+   <link rel="preconnect" href="https://fonts.googleapis.com" />
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+   <link
+      href="https://fonts.googleapis.com/css2?family=Athiti:wght@200;300;400;500;600;700&family=Life+Savers:wght@400;700;800&display=swap"
+      rel="stylesheet" />
+   <link rel="stylesheet" href="CSS/output.css">
+   <script src="main.js" defer></script>
+   <title>KANBAN</title>
+</head>
+
+<body
+   class="flex flex-col min-h-screen container pt-20 max-w-5xl mx-auto px-2 sm:px-4 lg:px-6 font-athiti bg-black">
