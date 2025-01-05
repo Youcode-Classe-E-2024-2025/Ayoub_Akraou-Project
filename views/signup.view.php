c4e844d865d012c747cc475f14e5de03ed54b6c7
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-01-05 20:00:14 +0100

    creer la page sign up static

---
+++ views/signup.view.php
@@ -0,0 +1,38 @@
+<?php require "views/components/head.php"; ?>
+<?php require "views/components/nav.php"; ?>
+<main class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 ">
+   <h2 class=" text-center text-2xl/9 font-bold tracking-tight text-white">Create an account</h2>
+   <div class="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form class="space-y-3" action="/signup" method="POST">
+         <?= isset($error) ? "<div class='bg-red-200 text-red-600 text-center'>-- $error --</div>" : '' ?>
+         <div>
+            <label for="fullname" class="block text-sm/6 font-medium text-black">fullname</label>
+            <input value="<?= isset($fullname) ? $fullname : '' ?>" type="text" name="fullname" id="fullname" autocomplete="fullname" placeholder="fullname" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-black outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
+         </div>
+         <div>
+            <label for="email" class="block text-sm/6 font-medium text-black">Email address</label>
+            <input value="<?= isset($email) ? $email : '' ?>" type="email" name="email" id="email" autocomplete="email" placeholder="email" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-black outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
+         </div>
+
+         <div>
+            <label for="password" class="block text-sm/6 font-medium text-black">Password</label>
+            <input type="password" name="password" id="password" autocomplete="current-password" placeholder="password" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-black outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
+         </div>
+         <div>
+            <label for="confirm-password" class="block text-sm/6 font-medium text-black">Confirm-password</label>
+            <input type="password" name="confirm-password" id="confirm-password" autocomplete="confirm-password" placeholder="confirm-password" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-black outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
+         </div>
+
+         <div>
+            <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+         </div>
+      </form>
+
+      <p class="mt-10 text-center text-sm/6 text-gray-500">
+         Already have an account?
+         <a href="/login" class="font-semibold text-indigo-600 hover:text-indigo-500">Login</a>
+      </p>
+
+   </div>
+</main>
+<?php require "views/components/footer.php"; ?>
