cab8eb8002c0ea87e6e4e9221003e18eb321a5ac
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-01-05 19:59:41 +0100

    modifier les styles globales

--- helpers/functions.php
+++ helpers/functions.php
@@ -1,7 +1,7 @@
 <?php
 function dd($value)
 {
-   echo '<pre class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-xl font-medium min-w-56 bg-black z-40 w-fit shadow-md shadow-white p-4 max-w-full text-wrap">';
+   echo '<pre class="!absolute top-20 left-1/2 -translate-x-1/2 text-xl font-medium min-w-56 bg-black text-white z-40 w-fit shadow-md shadow-white p-4 max-w-full text-wrap">';
    var_dump($value);
    echo '</pre>';
    // die();
