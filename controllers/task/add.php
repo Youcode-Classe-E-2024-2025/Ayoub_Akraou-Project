759812299f7a3676d57a47900c0959c116e2b3fe
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-01-06 21:04:50 +0100

    implementer la fonctionnalité d'ajouter une tache et assigner le aux membres

--- controllers/task/add.php
+++ controllers/task/add.php
@@ -1,1 +1,21 @@
 <?php
+session_start();
+require "../../helpers/functions.php";
+require "../../db/Database.php";
+require "../../models/Task.php";
+
+dd($_POST);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+   $title = htmlspecialchars($_POST["title"]);
+   $description = htmlspecialchars($_POST["description"]);
+   $due_date = htmlspecialchars($_POST["due_date"]);
+   $status = htmlspecialchars($_POST["status"]);
+   $category_id = htmlspecialchars($_POST["category"]);
+   $tags = array_map(fn($el) => htmlspecialchars($el), $_POST["tags"]);
+   $collaborators = array_map(fn($el) => htmlspecialchars($el), $_POST["collaborators"]);
+   dd($_SESSION);
+   $newTask = new Task($title, $description, $due_date, $status);
+   $newTask->createTask($_SESSION['project_id'], $category_id, $tags, $collaborators);
+   header('Location: /');
+}
