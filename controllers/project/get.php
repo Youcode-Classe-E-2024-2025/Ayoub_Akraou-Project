bf9c2f3c431d2bc46fce153e06e9aadf7614bdab
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-01-10 09:58:08 +0100

    creer le code pour recuperer les details d'un projet

--- controllers/project/get.php
+++ controllers/project/get.php
@@ -1,1 +1,10 @@
 <?php
+session_start();
+require "../../db/Database.php";
+require "../../models/Project.php";
+header('Content-Type: application/json');
+// echo json_encode($_GET['id']);
+if (isset($_GET['id'])) {
+   echo json_encode(Project::getDetails($_GET['id']));
+   exit;
+}
