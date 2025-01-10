ab487aafa2c7e820871e0003b010b90f552df5cd
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-01-10 09:16:15 +0100

    creer le code pour recuperer les donner d'une tache

--- controllers/task/get.php
+++ controllers/task/get.php
@@ -1,1 +1,11 @@
 <?php
+require "../../db/Database.php";
+require "../../models/Task.php";
+require "../../helpers/functions.php";
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+   $id = $_GET['id'];
+   $task = Task::getTaskDetails($id);
+   echo json_encode($task);
+   exit;
+}
